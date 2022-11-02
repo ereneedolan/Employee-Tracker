40c218f0d2b49a04d16ef651ea74eb420c030075
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-02 10:53:46 -0400

    added to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,28 +4,25 @@ VALUES ('Engineering'),
        ('Legal'),
        ('Sales'),
        ('HR'),
-       ('')
+       ('Operations');
 
 
 INSERT INTO roles (role_title, role_salary, department_id)
-VALUES ('Software Engineer', '', ''),
-       ('Account Manager', '', ''),
-       ('Accountant', '', ''),
-       ('Legal Team Lead', '', ''),
-       ('Lawyer', '', ''),
-       ('', '', ''),
-       ('', '', ''),
-       ('', '', ''),
-       ('', '', ''),
-       ('', '', ''),
-       ('', '', '')
+VALUES ('Software Engineer', 120000, ),
+       ('Account Manager', 160000, ),
+       ('Accountant', 125000, ),
+       ('Legal Team Lead', 250000, ),
+       ('Lawyer', 190000, ),
+       ('Sales Lead', 100000, ),
+       ('Salesperson', 80000, ),
+       ('Lead Engineer', 150000, ),
+       ('HR Manager', 80000, ),
+       ('Head of Operations', 95000, );
 
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('Eileen', 'Dolan', '', ''),
-       ('Mathew', 'Dolan', '', ''),
-       ('Russell', 'Dolan', '', ''),  
-       ('John', 'Smith', '', ''),
-       ('', '', '', ''), 
-       ('Eileen', 'Dolan', '', ''),
-       ('Eileen', 'Dolan', '', '')
+VALUES ('Eileen', 'Dolan', , ),
+       ('Mathew', 'Dolan', , ),
+       ('Russell', 'Dolan', , ),  
+       ('John', 'Smith', , );
+       
