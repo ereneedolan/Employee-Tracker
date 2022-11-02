64d854e2c29fdc4e4933c92e999eb1b0393a3d60
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-02 10:40:54 -0400

    started populating seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,31 @@
 INSERT INTO departments (department_name)
-VALUES (''),
-       (''),
+VALUES ('Engineering'),
+       ('Finance'),
+       ('Legal'),
+       ('Sales'),
+       ('HR'),
+       ('')
 
 
 INSERT INTO roles (role_title, role_salary, department_id)
-VALUES (''),
-       (''),
+VALUES ('Software Engineer', '', ''),
+       ('Account Manager', '', ''),
+       ('Accountant', '', ''),
+       ('Legal Team Lead', '', ''),
+       ('Lawyer', '', ''),
+       ('', '', ''),
+       ('', '', ''),
+       ('', '', ''),
+       ('', '', ''),
+       ('', '', ''),
+       ('', '', '')
 
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES (''),
-       (''),       
+VALUES ('Eileen', 'Dolan', '', ''),
+       ('Mathew', 'Dolan', '', ''),
+       ('Russell', 'Dolan', '', ''),  
+       ('John', 'Smith', '', ''),
+       ('', '', '', ''), 
+       ('Eileen', 'Dolan', '', ''),
+       ('Eileen', 'Dolan', '', '')
